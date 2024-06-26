e110bd5b79bd281e2319abe30cba87ea0b706a39
espark-kashafz <kashafzaidi60@gmail.com>
2024-06-26 16:08:07 +0500

    background image added and app name changed

--- src/component/weatherApp.jsx
+++ src/component/weatherApp.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import {
-  TextField,
-  Typography,
   Box,
-  CircularProgress,
-  IconButton,
-  InputAdornment,
   Paper,
+  Typography,
+  TextField,
+  InputAdornment,
+  IconButton,
+  CircularProgress,
 } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
+import { Search as SearchIcon } from '@mui/icons-material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFrown } from '@fortawesome/free-solid-svg-icons';
-import axios from 'axios';
-// import '../App.css'; // Assuming you have a CSS file for styling
 
 const WeatherUpdate = () => {
   const [input, setInput] = useState('');
@@ -21,7 +20,7 @@ const WeatherUpdate = () => {
     data: {},
     error: false,
   });
-  const [tomorrowWeather, setTomorrowWeather] = useState(null);
+  const [tenDayForecast, setTenDayForecast] = useState([]);
 
   const toDateFunction = () => {
     const months = [
@@ -53,9 +52,8 @@ const WeatherUpdate = () => {
     }`;
     return date;
   };
-
-  const search = async (event) => {
-    if (event.key === 'Enter') {
+  const search = async (event, isButtonClick = false) => {
+    if (isButtonClick || (event.key && event.key === 'Enter')) {
       event.preventDefault();
       setInput('');
       setWeather({ ...weather, loading: true });
@@ -70,9 +68,8 @@ const WeatherUpdate = () => {
           },
         })
         .then((res) => {
-          console.log('res', res);
           setWeather({ data: res.data, loading: false, error: false });
-          fetchTomorrowForecast(res.data.coord.lat, res.data.coord.lon);
+          fetchTenDayForecast(res.data.coord.lat, res.data.coord.lon);
         })
         .catch((error) => {
           setWeather({ ...weather, data: {}, error: true });
@@ -81,8 +78,7 @@ const WeatherUpdate = () => {
         });
     }
   };
-  console.log('weather', weather);
-  const fetchTomorrowForecast = async (lat, lon) => {
+  const fetchTenDayForecast = async (lat, lon) => {
     const url = `https://api.openweathermap.org/data/2.5/onecall`;
     const api_key = 'f00c38e0279b7bc85480c3fe775d518c';
     const exclude = 'current,minutely,hourly';
@@ -97,21 +93,17 @@ const WeatherUpdate = () => {
         },
       })
       .then((res) => {
-        console.log('tomorrow forecast res', res);
-        if (res.data && res.data.daily && res.data.daily.length >= 2) {
-          // Assuming res.data.daily[1] gives the forecast for tomorrow
-          setTomorrowWeather(res.data.daily[1]);
+        if (res.data && res.data.daily) {
+          setTenDayForecast(res.data.daily.slice(0, 10));
         }
       })
       .catch((error) => {
-        console.log('tomorrow forecast error', error);
-        setTomorrowWeather(null);
+        console.log('ten-day forecast error', error);
+        setTenDayForecast([]);
       });
   };
-
   return (
     <Box
-      className='App'
       component={Paper}
       elevation={3}
       p={4}
@@ -120,27 +112,32 @@ const WeatherUpdate = () => {
       justifyContent='center'
       alignItems='center'
       sx={{
-        width: '600px',
-        minHeight: '440px',
+        width: '80%',
+        minHeight: '640px',
         margin: '128px auto',
         borderRadius: '10px',
+        backgroundImage: "url('images/sky.jpg')",
+        backgroundRepeat: 'no-repeat',
       }}
     >
-      <Typography variant='h4' className='app-name' gutterBottom>
-        Today's Forecast
+      <Typography
+        variant='h4'
+        sx={{ color: 'black', padding: '6px', fontFamily: 'cursive' }}
+      >
+        Weather Updates
       </Typography>
-      <Box className='search-bar' width='100%' maxWidth='400px' mb={4}>
+      <Box width='100%' maxWidth='400px' padding='20px'>
         <TextField
           variant='outlined'
           fullWidth
           placeholder='Enter City Name'
           value={input}
           onChange={(event) => setInput(event.target.value)}
-          onKeyPress={search}
+          onKeyPress={(event) => search(event)}
           InputProps={{
             endAdornment: (
               <InputAdornment position='end'>
-                <IconButton onClick={search}>
+                <IconButton onClick={(event) => search(event, true)}>
                   <SearchIcon />
                 </IconButton>
               </InputAdornment>
@@ -154,24 +151,36 @@ const WeatherUpdate = () => {
         </Box>
       )}
       {weather.error && (
-        <Box mt={4} className='error-message'>
+        <Box mt={4}>
           <FontAwesomeIcon icon={faFrown} />
           <span style={{ fontSize: '20px', marginLeft: '10px' }}>
             City not found
           </span>
         </Box>
       )}
-      <Box sx={{ display: 'flex' }}>
+      <Box
+        sx={{
+          mx: 'auto',
+          border: '1px solid #fff',
+          width: '400px',
+          textAlign: 'center',
+          background: '#fff',
+          padding: '6px',
+        }}
+      >
+        <Typography variant='h5' sx={{ color: 'black', fontWeight: 'bold' }}>
+          Today's Forecast
+        </Typography>
         {weather.data && weather.data.main && (
           <Box mt={4}>
-            <Typography variant='h5' className='city-name'>
+            <Typography variant='h5'>
               {weather.data.name}, <span>{weather.data.sys.country}</span>
             </Typography>
             <Typography variant='body1' className='date'>
               {toDateFunction()}
             </Typography>
-            <Box className='weather-info'>
-              <div className='icon-temp'>
+            <Box>
+              <Box>
                 <img
                   src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}.png`}
                   alt={weather.data.weather[0].description}
@@ -180,44 +189,89 @@ const WeatherUpdate = () => {
                   {Math.round(weather.data.main.temp)}
                   <sup className='deg'>°C</sup>
                 </Typography>
-              </div>
-              <div className='des-wind'>
+              </Box>
+              <Box>
                 <Typography variant='body1'>
                   {weather.data.weather[0].description.toUpperCase()}
                 </Typography>
                 <Typography variant='body1'>
                   Wind Speed: {weather.data.wind.speed} m/s
                 </Typography>
-              </div>
-            </Box>
-          </Box>
-        )}
-        {tomorrowWeather && (
-          <Box mt={4}>
-            <Typography variant='h5'>Tomorrow's Forecast</Typography>
-            <Box className='weather-info'>
-              <div className='icon-temp'>
-                <img
-                  src={`https://openweathermap.org/img/wn/${tomorrowWeather.weather[0].icon}.png`}
-                  alt={tomorrowWeather.weather[0].description}
-                />
-                <Typography variant='h6'>
-                  {Math.round(tomorrowWeather.temp.day)}
-                  <sup className='deg'>°C</sup>
-                </Typography>
-              </div>
-              <div className='des-wind'>
-                <Typography variant='body1'>
-                  {tomorrowWeather.weather[0].description.toUpperCase()}
-                </Typography>
-                <Typography variant='body1'>
-                  Wind Speed: {tomorrowWeather.wind_speed} m/s
-                </Typography>
-              </div>
+              </Box>
             </Box>
           </Box>
         )}
       </Box>
+      {tenDayForecast.length > 0 && (
+        <Box
+          sx={{
+            width: '90%',
+            padding: '10px',
+          }}
+          mt={4}
+        >
+          <Typography
+            variant='h4'
+            sx={{
+              textAlign: 'center',
+              fontFamily: 'cursive',
+              paddingBottom: '15px',
+            }}
+          >
+            10-Day Forecast
+          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              overflow: 'auto',
+              scrollbarWidth: 'thin',
+              scrollbarColor: 'transparent transparent',
+              '&::-webkit-scrollbar': {
+                display: 'none',
+              },
+            }}
+          >
+            {tenDayForecast.map((day, index) => (
+              <Box
+                key={index}
+                className='weather-info'
+                sx={{
+                  border: '1px solid #F6F6F6',
+                  padding: '10px',
+                  textAlign: 'center',
+                  borderBottom: '10px solid #fff ',
+                }}
+              >
+                <Typography variant='h6'>
+                  {new Date(day.dt * 1000).toLocaleDateString('en-US', {
+                    weekday: 'long',
+                    month: 'long',
+                    day: 'numeric',
+                  })}
+                </Typography>
+                <Box>
+                  <img
+                    src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                    alt={day.weather[0].description}
+                  />
+                  <Typography variant='h6'>
+                    {Math.round(day.temp.day)}
+                    <sup className='deg'>°C</sup>
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='body1'>
+                    {day.weather[0].description.toUpperCase()}
+                  </Typography>
+                  <Typography variant='body1'>
+                    Wind Speed: {day.wind_speed} m/s
+                  </Typography>
+                </Box>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 };
